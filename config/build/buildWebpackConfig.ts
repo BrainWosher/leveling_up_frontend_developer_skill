007799707915ceffa061517f72e0cb6a11a176c4
BrainWosher <svprokopenko92@gmail.com>
2026-01-20 21:27:23 +0700

    fsd + абсолютные импорты

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -20,7 +20,7 @@ export function buildWebpackConfigt(options: BuildOptions): webpack.Configuratio
     module: {
       rules: buildLoaders(options),
     },
-    resolve: buildResolvers(),
+    resolve: buildResolvers(options),
     devtool: isDev ? 'inline-source-map' : undefined,
     devServer: isDev ? buildDevServer(options) : undefined,
   };
