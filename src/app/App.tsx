f5bbbf6e4c822195bf9aa1ee3735eb158bb749db
BrainWosher <svprokopenko92@gmail.com>
2026-01-21 22:54:26 +0700

    виджет navBar, AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,25 +1,17 @@
-import { Link, Route, Routes } from 'react-router-dom';
 import './styles/index.scss';
-import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
+import { NavBar } from 'widgets/NavBar';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
+      <NavBar />
+      <AppRouter />
       <button onClick={toggleTheme}>Toggle</button>
-      <Link to="/">Главная</Link>
-      <Link to="/about">О сайте</Link>
-      <Suspense fallback={<div> Loading...</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />}></Route>
-          <Route path="/" element={<MainPage />}></Route>
-        </Routes>
-      </Suspense>
     </div>
   );
 };
