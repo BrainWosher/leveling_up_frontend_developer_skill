be79bedca99e29781e0c169e2b35de70e79acb5f
BrainWosher <svprokopenko92@gmail.com>
2026-01-22 19:15:47 +0700

    sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,8 @@ import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
-import { NavBar } from 'widgets/NavBar';
+import { NavBar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
 const App = () => {
   const { theme } = useTheme();
@@ -10,7 +11,10 @@ const App = () => {
   return (
     <div className={classNames('app', {}, [theme])}>
       <NavBar />
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -1,16 +1,15 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher/indes';
-import cls from './NavBar.module.scss';
+import cls from './Navbar.module.scss';
 
-interface NavBarProps {
+interface NavbarProps {
   className?: string;
 }
 
-export const NavBar = ({ className }: NavBarProps) => {
+export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.navbar, {}, [className])}>
-      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
           Главная
