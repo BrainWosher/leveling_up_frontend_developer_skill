bc97d87f01621c8c8e766f571ae3b6bb2f962c08
BrainWosher <svprokopenko92@gmail.com>
2026-03-17 21:14:21 +0700

    add pre commit

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,9 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss';
-import {useTranslation} from "react-i18next";
-import Modal from "shared/ui/Modal/Modal";
-import {useCallback, useState} from "react";
-import {Button, ButtonTheme} from "shared/ui/Button/Button";
+import { useTranslation } from "react-i18next";
+import { useCallback, useState } from "react";
+import { Button, ButtonTheme } from "shared/ui/Button/Button";
 import { LoginModal } from "features/AuthByUsername";
 
 interface NavbarProps {
